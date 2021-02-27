117709aedca517689aafbe910a8cc6708bd9ab1f
Tushar <tushar1710vg@gmail.com>
2021-02-27 15:53:50 +0530

    Models Added

---
+++ app/src/main/java/com/msu/footprints/models/Sponsor.java
@@ -0,0 +1,22 @@
+package com.msu.footprints.models;
+
+import java.io.Serializable;
+
+public class Sponsor implements Serializable{
+    private String ImageURL="";
+
+    public  Sponsor(){
+    }
+
+    public Sponsor(String imageURL){
+        ImageURL = imageURL;
+    }
+
+    public String getImageURL(){
+        return ImageURL;
+    }
+
+    public void setImageURL(String imageURL){
+        ImageURL = imageURL;
+    }
+}
